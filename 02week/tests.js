2443e6bad9f647d6b0bc530e4e57d89bb46f9cac
Rob W <robpwatkins@gmail.com>
2019-11-03 21:54:31 -0600

    Updated tests

--- 02week/tests.js
+++ 02week/tests.js
@@ -21,6 +21,10 @@ function rockPaperScissors(hand1, hand2) {
     return "It's a tie!";
   } else if (hand1 === 'rock' && hand2 === 'scissors') {
     return "Hand one wins!";
+  } else if (hand1 === 'paper' && hand2 === 'rock') {
+    return "Hand one wins!";
+  } else if (hand1 === 'scissors' && hand2 === 'paper') {
+    return "Hand one wins!";
   } else if (hand1 === 'paper' && hand2 === 'scissors') {
     return "Hand two wins!";
   } else if (hand1 === 'scissors' && hand2 === 'rock') {
@@ -57,10 +61,18 @@ if (typeof describe === 'function') {
       assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
       assert.equal(rockPaperScissors('scissors', 'rock'), "Hand two wins!");
     });
+    it('should detect which hand won', () => {
+      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
+      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+    });
     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
       assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
       assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
       assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+    });
+    it('should check to make sure user entry is valid', () => {
+      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
     });
   });
 } else {
