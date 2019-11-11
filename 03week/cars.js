615cfbc6c863c6d97f37a277b476e9f05034dc37
Rob W <robpwatkins@gmail.com>
2019-11-10 19:17:56 -0600

    Finished and added Array of Cars to a js file

--- 03week/cars.js
+++ 03week/cars.js
@@ -1,125 +1,87 @@
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+// length
+// Create an array called cars which consists of 4 different types of cars. The first car type should be Ford.
+// Console.log the length of the array.
+let cars = ['Ford', 'Audi', 'Jeep', 'Chrysler'];
+console.log(cars.length);
 
-let playerTurn = 'X';
+// concat
+// Create another array called more cars with 4 more different types of cars. The last car type should be Honda.
+// Use the concat method to combine the cars and moreCars arrays into an array called totalCars.
+let moreCars = ['Dodge', 'Mitsubishi', 'Volkswagen', 'Honda'];
+let totalCars = (cars.concat(moreCars));
+console.log(totalCars);
 
-function printBoard() {
-  console.log('   0  1  2');
-  console.log('0 ' + board[0].join(' | '));
-  console.log('  ---------');
-  console.log('1 ' + board[1].join(' | '));
-  console.log('  ---------');
-  console.log('2 ' + board[2].join(' | '));
+// indexOf and lastIndexOf
+// Use the indexOf method to console.log the index of Honda.
+console.log(totalCars.indexOf('Honda'));
+
+// Use the lastIndexOf method to console.log the index of Ford.
+console.log(totalCars.lastIndexOf('Ford'));
+
+// join
+// Use the join method to convert the array totalCars into a string called stringOfCars.
+let stringOfCars = totalCars.toString();
+console.log(stringOfCars);
+
+// split
+// Use the split method to convert stringOfCars back into an array called totalCars.
+totalCars = stringOfCars.split();
+
+// reverse
+// Use the reverse method to create an array carsInReverse which is the array totalCars in reverse.
+let carsInReverse = totalCars.reverse();
+console.log(carsInReverse);
+
+// sort
+// Use the sort method to put the array carsInReverse into alphabetical order.
+// Based on the types of cars you used, predict which item in the array should be at index 0.
+// Use the following code to confirm or reject your prediction:
+// alert(carsInReverse.indexOf('yourPrediction'));
+console.log(carsInReverse.sort());
+
+alert(carsInReverse.indexOf('Chrysler'));
+
+// Use the slice method to remove Ford and Honda from the carsInReverse array and move them into a new array called removedCars.
+
+let removedCars = carsInReverse.slice(3, 5);
+console.log(removedCars);
+
+// splice
+// Use the splice method to remove the 2nd and 3rd items in the array carsInReverse and add Ford and Honda in their place.
+
+carsInReverse.splice(1, 2, 'Ford', 'Honda');
+console.log(carsInReverse);
+
+// push
+// Use the push method to add the types of cars that you removed using the splice method to the carsInReverse array.
+
+carsInReverse.push('Chrysler', 'Dodge');
+console.log(carsInReverse);
+
+// pop
+// Use the pop method to remove and console.log the last item in the array carsInReverse.
+
+console.log(carsInReverse.pop());
+
+// shift
+// Use the shift method to remove and console.log the first item in the array carsInReverse.
+
+console.log(carsInReverse.shift());
+
+// unshift
+// Use the unshift method to add a new type of car to the array carsInReverse.
+
+carsInReverse.unshift('Tesla');
+console.log(carsInReverse);
+
+// forEach
+// Create an array called numbers with the following items: 23, 45, 0, 2 Write code that will add 2 to each item in the array numbers.
+// .forEach() requires a function to be passed into it as its first argument. Build a function that will add 2 and then use .forEach() to pass each number into your freshly built function. const numbers = [23, 45, 0 , 2, 8, 44, 100, 1, 3, 91, 34]
+
+const numbers = [23, 45, 0, 2, 8, 44, 100, 1, 3, 91, 34];
+function addTwo(item, index, arr) {
+  arr[index] = item + 2;
 }
+numbers.forEach(addTwo);
 
-function test() {
-  console.log('test');
-}
-
-function horizontalWin() {
-  // Your code here
-  // Check the rows of the board for a match
-  if (
-    board [0][0] === playerTurn && 
-    board [0][1] === playerTurn && 
-    board [0][2] === playerTurn
-  ) { 
-    return true;
-  } else if (
-    board [1][0] === playerTurn && 
-    board [1][1] === playerTurn && 
-    board [1][2] === playerTurn
-  ) {
-    return true;
-  } else if (
-    board [2][0] === playerTurn && 
-    board [2][1] === playerTurn && 
-    board [2][2] === playerTurn
-  ) {
-    return true;
-  }
-    return false;
-}
-
-function verticalWin() {
-  // Your code here
-  // Check the columns of the board
-  if (
-    board [0][0] === playerTurn && 
-    board [1][0] === playerTurn && 
-    board [2][0] === playerTurn
-  ) { 
-    return true;
-  } else if (
-    board [0][1] === playerTurn && 
-    board [1][1] === playerTurn && 
-    board [2][1] === playerTurn
-  ) {
-    return true;
-  } else if (
-    board [0][2] === playerTurn && 
-    board [1][2] === playerTurn && 
-    board [2][2] === playerTurn
-  ) {
-    return true;
-  }
-    return false;
-}
-
-function diagonalWin() {
-  // Your code here
-  // Check for a line pattern top left to bottom right on the board
-  // "" top right to bottom left
-  if (
-    board [0][0] === playerTurn && 
-    board [1][1] === playerTurn && 
-    board [2][2] === playerTurn
-  ) { 
-    return true;
-  } else if (
-    board [0][2] === playerTurn && 
-    board [1][1] === playerTurn && 
-    board [2][0] === playerTurn
-  ) {
-    return true;
-  }
-    return false;
-}
-
-function checkForWin() {
-  // Your code here
-  // Check for horizontal, vertical, and diagonal wins
-  // Return true if h or v or d wins
-
-  if (verticalWin() || horizontalWin() || diagonalWin()) {
-  return true;
-}
-}
-
-function ticTacToe(row, column) {
-  // Manipulate the board array (place X or O) based on user input (row, column)
-  let selectedRow = board[row];
-  selectedRow[column] = playerTurn;
-
-// Manipulate playerTurn variable, switch from X to O or 0 to X
-if (playerTurn === 'X') {
-  playerTurn = 'O';
-} else if  (playerTurn === 'O') {
-  playerTurn = 'X';
-}
-}
-
-function getPrompt() {
-  printBoard();
-  console.log("It's Player " + playerTurn + "'s turn.");
-  rl.question('row: ', (row) => {
-    rl.question('column: ', (column) => {
-      ticTacToe(row, column);
-      getPrompt();
-    });
-  });
-
-}
+console.log(numbers);
