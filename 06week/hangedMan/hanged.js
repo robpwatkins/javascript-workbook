4b1256b9cfc490d62ae9be69157eda779955f2db
Rob W <robpwatkins@gmail.com>
2019-12-07 21:42:03 -0600

    Almost there

--- 06week/hangedMan/hanged.js
+++ 06week/hangedMan/hanged.js
@@ -5,3 +5,24 @@ function buttonClicked () {
 }
 
 document.getElementById('button').addEventListener('click', buttonClicked);
+
+let targetPhrase = 'Javascript is for winners'
+let targetArray = targetPhrase.split('');
+let targetHint = [];
+let inputLetter = 'r';
+
+for (let i = 0; i < targetPhrase.length; i++) {
+  if (targetPhrase[i] === ' ') {
+    targetHint[i] = '&nbsp' + '&nbsp' + '&nbsp' + '&nbsp';
+  } else {
+    if (inputLetter === targetPhrase[i]) {
+      targetHint[i] = inputLetter;
+    } else {
+    targetHint[i] = ' ___ ' ;
+    }
+  }
+}
+
+console.log(targetHint.join(''));
+
+document.getElementById('hangedText').innerHTML = targetHint.join('');
