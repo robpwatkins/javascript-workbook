5af347a8e01c4da2ddf83547f1d49ea1a7252c3b
Rob W <robpwatkins@gmail.com>
2019-12-08 09:40:32 -0600

    Donnnnneeee

--- 06week/hangedMan/hanged.js
+++ 06week/hangedMan/hanged.js
@@ -1,24 +1,43 @@
 let targetPhrase = 'javascript is for winners'
 let targetHint = [];
+let guessesLeft = 5;
 
 for (let i = 0; i < targetPhrase.length; i++) {
   if (targetPhrase[i] === ' ') {
-    targetHint[i] = '&nbsp' + '&nbsp' + '&nbsp' + '&nbsp';
+    targetHint[i] = '&nbsp' + '&nbsp';
   } else {
-    targetHint[i] = ' ___ ' ;
+    targetHint[i] = '_' ;
   }
 }
 
 document.getElementById('hangedText').innerHTML = targetHint.join('');
+document.getElementById('guessesLeft').innerHTML = `INCORRECT GUESSES LEFT: ${guessesLeft}`;
 
 function buttonClicked () {
   let inputLetter = document.getElementById('input').value;
+  let number = 0;
   for (let i = 0; i < targetPhrase.length; i++) {
     if (inputLetter === targetPhrase[i]) {
-      targetHint[i] = '&nbsp' + '&nbsp' + inputLetter + '&nbsp' + '&nbsp';
-    }
+      targetHint[i] = inputLetter;
+    } else 
+    number++;
+    // return number;
   }
+  if (number > 24) {
+    guessesLeft--;
+  }
+  document.getElementById('guessesLeft').innerHTML = `INCORRECT GUESSES LEFT: ${guessesLeft}`;
+  document.getElementById('input').value = '';
   document.getElementById('hangedText').innerHTML = targetHint.join('');
+  if (guessesLeft <= 0) {
+    document.getElementById('guessesLeft').innerHTML = 'GAME OVER!';
+  }
+}
+
+
+function showAnswer () {
+  document.getElementById('theAnswer').style.display = 'block';
 }
 
 document.getElementById('button').addEventListener('click', buttonClicked);
+document.getElementById('answerButton').addEventListener('click', showAnswer);
